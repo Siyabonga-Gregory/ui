698c29d5bed8c5a38aecc2f548e94419036ee3c1
Siyabonga Gregory <huge.fuze@gmail.com>
2022-01-11 15:01:19 +0200

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import React from 'react';
 import { Button } from 'reactstrap';
+import NavBarV1 from './components/NavBarV1';
 
 function App() {
-  const [collapsed, setCollapsed] = useState(true);
-  const toggleNavbar = () => setCollapsed(!collapsed);
-
-
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <div>
-            <Navbar color="faded" light>
-              <NavbarBrand href="/" className="mr-auto">Ghost Nav</NavbarBrand>
-              <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-              <Collapse isOpen={!collapsed} navbar>
-                <Nav navbar>
-                  <NavItem>
-                    <NavLink href="/components/">Components</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                  </NavItem>
-                </Nav>
-              </Collapse>
-            </Navbar>
-          </div>
+          <NavBarV1/>
+          <Button color="danger">Hallo from Ghost Coder</Button>
         </div>
       </header>
     </div>
