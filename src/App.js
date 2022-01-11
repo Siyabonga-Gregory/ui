1b3304e636468dcf75c34cb1b5bb965798451df1
Siyabonga Gregory <huge.fuze@gmail.com>
2022-01-11 15:09:45 +0200

    Added ButtonsV1 Component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import React from 'react';
 import { Button } from 'reactstrap';
 import NavBarV1 from './components/NavBarV1';
+import ButtonsV1 from './components/ButtonsV1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <NavBarV1/>
-          <Button color="danger">Hallo from Ghost Coder</Button>
+        <div><Button color="danger">Hallo from Ghost Coder</Button>
+           <div><NavBarV1/></div><hr/>
+           <div> <ButtonsV1/></div> <hr/>
         </div>
       </header>
     </div>
