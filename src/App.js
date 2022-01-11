46b11fdb9165442af72a6d1dcd350bbe9e2fed59
Siyabonga Gregory <huge.fuze@gmail.com>
2022-01-11 15:00:43 +0200

    Basic Nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import { Button } from 'reactstrap';
 
 function App() {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <Button color="danger">Hallo from Ghost Coder</Button>
+          <div>
+            <Navbar color="faded" light>
+              <NavbarBrand href="/" className="mr-auto">Ghost Nav</NavbarBrand>
+              <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+              <Collapse isOpen={!collapsed} navbar>
+                <Nav navbar>
+                  <NavItem>
+                    <NavLink href="/components/">Components</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </div>
         </div>
       </header>
     </div>
