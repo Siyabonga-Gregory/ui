a94595636f5e3d642cfef1e79023ebf085b1415b
Siyabonga Gregory <huge.fuze@gmail.com>
2022-01-19 09:32:21 +0200

    Clean up

--- src/components/Cards/CardsV2.js
+++ src/components/Cards/CardsV2.js
@@ -6,29 +6,29 @@ import CardsV2Css from './CardsV2Css.css';
 export default function CardsV2() {
     return (<>
         <div className='cardDiv'>
-               <div className='CardTitle'>
-               Are you sure you want to stop the application process?
-                </div>
+            <div className='CardTitle'>
+                Are you sure you want to stop the application process?
+            </div>
             <div>
                 <Card inverse>
                     <CardImg
                         alt="Card image cap"
-                        src=""
+
                         width="100%"
                     />
-                    
-                        <CardImgOverlay>
-                            <CardText className='CardText'>
-                                Text here
-                            </CardText>
-                            <CardText className='CardText'>
-                               Text Here
-                            </CardText>
-                        </CardImgOverlay>
-                   
+
+                    <CardImgOverlay>
+                        <CardText className='CardText'>
+                            Text here
+                        </CardText>
+                        <CardText className='CardText'>
+                            Text here
+                        </CardText>
+                    </CardImgOverlay>
+
                     <div className='buttonsDiv'>
-                         <div><Button className='continueButton'>Continue with application online<i className='sign'></i></Button></div>
-                         <div><Button className='callMeBackButton'>Call me back<i className='sign'></i></Button></div>
+                        <div><Button className='continueButton'>Continue with application online<i className='sign'></i></Button></div>
+                        <div><Button className='callMeBackButton'>Call me back<i className='sign'></i></Button></div>
                     </div>
                 </Card>
             </div>
